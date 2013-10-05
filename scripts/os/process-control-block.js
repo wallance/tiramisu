5f053af2666236b4498d2b3f2a93531f1b976927
Wallance Miranda <wallancem@gmail.com>
2013-10-05 18:09:47 -0400

    Changed file name to reflect prototype name: ProcessControlBlock

---
+++ scripts/os/process-control-block.js
@@ -0,0 +1,10 @@
+/* 
+ * Process Control Block
+ * 
+ * The manager is responsible for communicating between the system's memory
+ * and the operating system.
+ */
+
+function ProcessControlBlock() {
+   
+}
