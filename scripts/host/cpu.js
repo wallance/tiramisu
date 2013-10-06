e6318e21132343449637f42cae7e3198375355f3
Wallance Miranda <wallancem@gmail.com>
2013-10-06 18:21:40 -0400

    Added an object array for translating operation codes to the appropriate executing method.  Added the skeleton methods for the corresponding opcode methods.

--- scripts/host/cpu.js
+++ scripts/host/cpu.js
@@ -21,18 +21,104 @@ function Cpu() {
     this.Zflag = 0;     // Z-ero flag (Think of it as "isZero".)
     this.isExecuting = false;
     
-    this.init = function() {
-        this.PC    = 0;
-        this.Acc   = 0;
-        this.Xreg  = 0;
-        this.Yreg  = 0;
-        this.Zflag = 0;      
-        this.isExecuting = false;  
+    // Based on the instruction set of the 6502 microprocessor.
+    // See: http://www.labouseur.com/courses/os/instructionset.pdf
+    this.OPERATION_CODES = {
+		'A9' : Cpu.prototype.loadAccumulatorWithConstant,
+                'AD' : Cpu.prototype.loadAccumulatorFromMemory,
+                '8D' : Cpu.prototype.storeAccumulatorInMemory,
+                '6D' : Cpu.prototype.addWithCarry,
+                'A2' : Cpu.prototype.loadRegisterXWithConstant,
+                'AE' : Cpu.prototype.loadRegisterXFromMemory,
+                'A0' : Cpu.prototype.loadRegisterYWithConstant,
+                'AC' : Cpu.prototype.loadRegosterYFromMemory,
+                'EA' : Cpu.prototype.noOperation,
+                '00' : Cpu.prototype.breakCall,
+                'EC' : Cpu.prototype.compareByteInMemoryWithRegisterX,
+                'D0' : Cpu.prototype.branchXBytesIfZFlagIsZero,
+                'EE' : Cpu.prototype.incrementValueOfAByte,
+                'FF' : Cpu.prototype.systemCall
     };
     
-    this.cycle = function() {
-        krnTrace("CPU cycle");
-        // TODO: Accumulate CPU usage and profiling statistics here.
-        // Do the real work here. Be sure to set this.isExecuting appropriately.
-    };
-}
+    this.init();    
+};
+
+Cpu.prototype.init = function() {
+    this.PC    = 0;
+    this.Acc   = 0;
+    this.Xreg  = 0;
+    this.Yreg  = 0;
+    this.Zflag = 0;      
+    this.isExecuting = false;
+};
+    
+
+/***************************
+ *
+ * OPERATION CODE METHODS
+ * 
+ ***************************/
+
+
+Cpu.prototype.loadAccumulatorWithConstant = function () {
+    
+};
+Cpu.prototype.loadAccumulatorFromMemory = function () {
+};
+Cpu.prototype.storeAccumulatorInMemory = function () {
+    
+};
+
+
+Cpu.prototype.addWithCarry = function () {
+    
+};
+Cpu.prototype.loadRegisterXWithConstant = function () {
+    
+};
+Cpu.prototype.loadRegisterXFromMemory = function () {
+    
+};
+Cpu.prototype.loadRegisterYWithConstant = function () {
+    
+};
+Cpu.prototype.loadRegosterYFromMemory = function () {
+    
+};
+Cpu.prototype.noOperation = function () {
+    
+};
+Cpu.prototype.breakCall = function () {
+    
+};
+Cpu.prototype.compareByteInMemoryWithRegisterX = function () {
+    
+};
+Cpu.prototype.branchXBytesIfZFlagIsZero = function () {
+    
+};
+Cpu.prototype.incrementValueOfAByte = function () {
+    
+};
+Cpu.prototype.systemCall = function () {
+    
+};
+
+Cpu.prototype.setCPUProperties = function(programCounter, accumulator, xReg, yReg, zFlag) {
+        this.PC     = programCounter;
+        this.Acc    = accumulator;
+        this.Xreg   = xReg;
+        this.Yreg   = yReg;
+        this.Zflag  = zFlag;
+};
+    
+// Called when isExecuting is true
+Cpu.prototype.cycle = function() {
+    krnTrace("CPU cycle");
+    // TODO: Accumulate CPU usage and profiling statistics here.
+    // Do the real work here. Be sure to set this.isExecuting appropriately.
+
+
+    // Refresh the CPU display
+    UIUpdateManager.updateCPUMonitor();
+};
