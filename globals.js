ba64d65ca0f37cc20a3319bcaf64e74f5ab65cc9
Wallance Miranda <wallancem@gmail.com>
2013-10-06 17:07:29 -0400

    Added the _PCBFactory global variable.

--- globals.js
+++ globals.js
@@ -12,13 +12,22 @@
 // Global CONSTANTS
 //
 var APP_NAME = "Tiramisu";  // 'cause I was at a loss for a better name.
-var APP_VERSION = "0.01";   // What did you expect?
+var APP_VERSION = "0.02";   // What did you expect?
 
 var CPU_CLOCK_INTERVAL = 100;   // This is in ms, or milliseconds, so 1000 = 1 second.
 
 var TIMER_IRQ = 0;  // Pages 23 (timer), 9 (interrupts), and 561 (interrupt priority).
                     // NOTE: The timer is different from hardware/host clock pulses. Don't confuse these.
 var KEYBOARD_IRQ = 1;  
+
+// System Memory (size is in bytes)
+// The size of a block to execute user programs
+var SYSTEM_MEMORY_BLOCK_SIZE = 256;
+// The number of blocks to use for user programs
+var SYSTEM_MEMORY_BLOCK_COUNT = 3;
+// Total size of the memory
+var SYSTEM_MEMORY_SIZE = SYSTEM_MEMORY_BLOCK_SIZE * SYSTEM_MEMORY_BLOCK_COUNT;
+
 
 
 //
@@ -29,6 +38,12 @@ var _CPU = null;
 var _OSclock = 0;       // Page 23.
 
 var _Mode = 0;   // 0 = Kernel Mode, 1 = User Mode.  See page 21.
+
+var _MemoryManager = null;
+
+// Processes
+var _PCBFactory = null;
+var _CurrentExecutingProcess = null;
 
 var _Canvas = null;               // Initialized in hostInit().
 var _DrawingContext = null;       // Initialized in hostInit().
