c6467c413adf2a7985fb73505415a60267136f25
Wallance Miranda <wallancem@gmail.com>
2013-10-09 13:52:18 -0400

    Created the initial PCB Factory that is responsible for creating new and keeping track of the current processes.

---
+++ scripts/os/pcb-factory.js
@@ -0,0 +1,76 @@
+/* 
+ * Process Control Block Factory
+ * 
+ * Responsible for creating and maintaining user processes.
+ */
+
+function ProcessControlBlockFactory() {
+    this.processes = null;
+    this.lastProcessID = 0; // Keeps track of the last PID used
+    
+    this.init();
+}
+
+ProcessControlBlockFactory.prototype.init = function() {
+    if ((this.processes === null)) {
+        this.processes = new Array();
+        this.systemMemory = new MemoryHardware(SYSTEM_MEMORY_SIZE);
+        
+    }
+};
+
+ProcessControlBlockFactory.prototype.getNextBaseAddress = function() {
+     if(this.processes.length <= SYSTEM_MEMORY_BLOCK_SIZE) {
+        return SYSTEM_MEMORY_BLOCK_SIZE * this.processes.length;
+    } else {
+        alert("ERROR base address!");
+    }
+};
+
+ProcessControlBlockFactory.prototype.getNextLimitAddress = function() {
+    if(this.processes.length <= SYSTEM_MEMORY_BLOCK_SIZE) {
+        // Subtract 1 because we started at 0.
+        return this.getNextBaseAddress() + SYSTEM_MEMORY_BLOCK_SIZE - 1;
+    } else {
+        alert("ERROR limit address!");
+    }
+};
+
+/*
+ * Only to be used when the PCB is instantiated, and only to be
+ * called once per process!
+ */
+ProcessControlBlockFactory.prototype.obtainNewProcessID = function() { 
+    return this.lastProcessID++;
+};
+
+ProcessControlBlockFactory.prototype.createProcess = function() {
+    
+    if (this.processes.length <= SYSTEM_MEMORY_BLOCK_COUNT) {
+        
+        var baseAddress = this.getNextBaseAddress();
+        var limitAddress = this.getNextLimitAddress();
+        var memoryBlock = this.processes.length + 1;
+        
+        var pcb = new ProcessControlBlock(this.obtainNewProcessID(), baseAddress, limitAddress, memoryBlock);
+        
+        this.processes[pcb.getProcessID()] = pcb;
+        
+        return this.processes[pcb.getProcessID()];
+    
+    } else {
+        alert("too many processes");
+    }
+};
+
+ProcessControlBlockFactory.prototype.removeProcess = function(block) {
+    
+};
+
+ProcessControlBlockFactory.prototype.getProcess = function(pid) {
+    if ( (typeof this.processes[pid] !== 'undefined') || (this.processes[pid] !== null) ) {
+        return this.processes[pid];
+    } else {
+        return null;
+    }
+};
