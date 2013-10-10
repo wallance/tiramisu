8fe3c1a896f290c5797f64246bb2f64c0413f7cb
Wallance Miranda <wallancem@gmail.com>
2013-10-10 00:23:47 -0400

    Added the functionality for the required op codes. Tested them with the glados test script.  Updates displays appropriately.

--- scripts/host/cpu.js
+++ scripts/host/cpu.js
@@ -24,20 +24,27 @@ function Cpu() {
     // Based on the instruction set of the 6502 microprocessor.
     // See: http://www.labouseur.com/courses/os/instructionset.pdf
     this.OPERATION_CODES = {
-		'A9' : Cpu.prototype.loadAccumulatorWithConstant,
-                'AD' : Cpu.prototype.loadAccumulatorFromMemory,
-                '8D' : Cpu.prototype.storeAccumulatorInMemory,
-                '6D' : Cpu.prototype.addWithCarry,
-                'A2' : Cpu.prototype.loadRegisterXWithConstant,
-                'AE' : Cpu.prototype.loadRegisterXFromMemory,
-                'A0' : Cpu.prototype.loadRegisterYWithConstant,
-                'AC' : Cpu.prototype.loadRegosterYFromMemory,
-                'EA' : Cpu.prototype.noOperation,
-                '00' : Cpu.prototype.breakCall,
-                'EC' : Cpu.prototype.compareByteInMemoryWithRegisterX,
-                'D0' : Cpu.prototype.branchXBytesIfZFlagIsZero,
-                'EE' : Cpu.prototype.incrementValueOfAByte,
-                'FF' : Cpu.prototype.systemCall
+        // After a few hours of figuring out why calling these methods
+        // were losing the 'this' scope, do not use the method itself as the value
+        // i.e: Cpu.prototype.loadAccumulatorFromMemory.  Instead, enclose just
+        // the method name in quotes, to store it as a string. To call the method,
+        // use: this[<variable-containing-method-name>], where
+        // <variable-containing-method-name> is the name of the method as returned
+        // by this array.
+        'A9' : 'loadAccumulatorWithConstant',
+        'AD' : 'loadAccumulatorFromMemory',
+        '8D' : 'storeAccumulatorInMemory',
+        '6D' : 'addWithCarry',
+        'A2' : 'loadRegisterXWithConstant',
+        'AE' : 'loadRegisterXFromMemory',
+        'A0' : 'loadRegisterYWithConstant',
+        'AC' : 'loadRegisterYFromMemory',
+        'EA' : 'noOperation',
+        '00' : 'breakCall',
+        'EC' : 'compareByteInMemoryWithRegisterX',
+        'D0' : 'branchXBytesIfZFlagIsZero',
+        'EE' : 'incrementValueOfAByte',
+        'FF' : 'systemCall'
     };
     
     this.init();    
@@ -51,7 +58,82 @@ Cpu.prototype.init = function() {
     this.Zflag = 0;      
     this.isExecuting = false;
 };
+
+/*
+ * Increments the PC by 1 each time method is called.
+ * Used when moving to the next instruction and moving to the next byte of
+ * memory.
+ */
+Cpu.prototype.incrementProgramCounter = function () {
+    this.PC++; // incremented on separate line for breakpoint/debugging purposes.
+    return this.PC;
+};
+
+/*
+ * Returns the current value of the PC.
+ */
+Cpu.prototype.getProgramCounterValue = function () {
+    return this.PC;  
+};
+
+/**
+ * Executes the next instruction, as specified by the program counter.
+ */
+Cpu.prototype.executeNextInstruction = function () {
     
+    // Determine next instruction
+    var nextInstruction = this.fetchNextInstructionFromMemory();
+    
+    // Determine corresponding method
+    var instructionMethod = this.OPERATION_CODES[nextInstruction];
+    
+    // Execute it, if it was a valid opcode
+    if ( (instructionMethod !== null) && (typeof instructionMethod !== 'undefined') ) {
+        
+        // This is how we must call the method name, while keeping the same
+        // context.  If we do not use the "this" keyword, any
+        // call in the format of: this.<method-or-property> will loose its scope.
+        // For details, see the comment above the OPERATION_CODES array.
+        // See: http://stackoverflow.com/questions/7652672/call-function-from-string-inside-object
+        this[instructionMethod]();
+    } else {
+        krnTrapError("Failed to execute an invalid operaton code: " + nextInstruction + ".");
+    }
+};
+
+Cpu.prototype.fetchNextByteFromMemory = function() {
+    // Increment PC because we are done reading the byte.  When the current
+    // executing instruction is finished, we need to make sure the program
+    // counter is pointing to the next instruction and not an operand.
+    // Some op codes have 1 byte operands, others have 2 bytes.  It is up to
+    // the individual op code methods to call this twice, if it does require
+    // a two byte operand.
+    var nextLogicalAddress = this.incrementProgramCounter();
+    
+    // What data is stored at the next logical address?
+    var nextByte = _MemoryManager.readDataAtLogicalAddress(nextLogicalAddress, _CurrentExecutingProcess);
+    
+    return nextByte;
+}
+
+Cpu.prototype.fetchNextInstructionFromMemory = function() {
+    // The program counter (PC) contains the next logical address
+    // of the next instruction.
+    var pc = _CPU.getProgramCounterValue();
+    
+    var nextInstruction = _MemoryManager.readDataAtLogicalAddress(pc, _CurrentExecutingProcess);
+    
+    var physicalAddress = _MemoryManager.translateAddress(pc, _CurrentExecutingProcess);
+    
+    physicalAddress = UIUpdateManager.baseTenToBaseSixteen(physicalAddress, false);
+    
+    // Added this because it is useful to know which address is currently executing.
+    // Using JS breakpoints in Chrome is another way to step through and understand which
+    // instruction is executing next.
+    $('#mem-address-' + physicalAddress).css("font-weight","Bold");
+    
+    return nextInstruction;
+};
 
 /***************************
  *
@@ -59,49 +141,344 @@ Cpu.prototype.init = function() {
  * 
  ***************************/
 
-
+/**
+ * Op Code: A9
+ * 
+ * Loads the accumulator with the constant, as specified by the operand.
+ */
 Cpu.prototype.loadAccumulatorWithConstant = function () {
     
+    // Obtain the constant from memory
+    var constantFromMemory = this.fetchNextByteFromMemory();
+    
+    // The second parameter (known as the radix) specifies the base of what
+    // the first parameter is supposed to be recognized as.
+    // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+    this.Acc = parseInt(constantFromMemory, 16);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+/**
+ * Op Code: AD
+ * 
+ * Loads the accumulator with the value specified at the address of the two byte
+ * operand.
+ */
 Cpu.prototype.loadAccumulatorFromMemory = function () {
+    
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
+    
+    // Format the bytes as a logical address
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16)); //.toString(16);
+    
+    // Read the data at the determined logical address.
+    this.Acc = _MemoryManager.readDataAtLogicalAddress(logicalAddress, _CurrentExecutingProcess);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+/**
+ * Op Code: 8D
+ * 
+ * 
+ */
 Cpu.prototype.storeAccumulatorInMemory = function () {
     
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
+    
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16));
+    
+    _MemoryManager.writeDataAtLogicalAddress(logicalAddress, this.Acc, _CurrentExecutingProcess);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
 
-
+/**
+ * Op Code: 6D
+ * 
+ * Adds the value located at the address specified by the two byte operand, and
+ * stores it in the accumulator.
+ */
 Cpu.prototype.addWithCarry = function () {
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
     
+    // Format the bytes as a logical address
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16));
+    
+    // Read the data at the determined logical address.
+    var additive = _MemoryManager.readDataAtLogicalAddress(logicalAddress, _CurrentExecutingProcess);
+    
+    this.Acc = (parseInt(this.Acc, 16) + parseInt(additive, 16));
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+/**
+ * Op Code: A2
+ * 
+ * Loads a constant, as specified by the one byte operand, into the Y register.
+ */
 Cpu.prototype.loadRegisterXWithConstant = function () {
+    // Obtain the constant from memory (as specified by the operand in memory)
+    var constantFromMemory = this.fetchNextByteFromMemory();
     
+    this.Xreg = parseInt(constantFromMemory, 16);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+/**
+ * Op Code: AE
+  * Loads the contents of memory, as specified by the two byte operand,
+ * into the X register.
+ */
 Cpu.prototype.loadRegisterXFromMemory = function () {
     
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
+    
+    // Format the bytes as a logical address
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16));
+    
+    // Read the data at the determined logical address.
+    this.Xreg = _MemoryManager.readDataAtLogicalAddress(logicalAddress, _CurrentExecutingProcess);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+/**
+ * Op Code: A0
+ * 
+ * Loads a constant, as specified by the one byte operand, into the Y register.
+ */
 Cpu.prototype.loadRegisterYWithConstant = function () {
     
-};
-Cpu.prototype.loadRegosterYFromMemory = function () {
+    // Obtain the constant from memory (as specified by the operand in memory)
+    var constantFromMemory = this.fetchNextByteFromMemory();
     
+    this.Yreg = parseInt(constantFromMemory, 16);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+/**
+ * Op Code: AC
+ * 
+ * Loads the contents of memory, as specified by the two byte operand,
+ * into the Y register.
+ */
+Cpu.prototype.loadRegisterYFromMemory = function () {
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
+    
+    // Format the bytes as a logical address
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16));
+    
+    // Read the data at the determined logical address.
+    this.Yreg = _MemoryManager.readDataAtLogicalAddress(logicalAddress, _CurrentExecutingProcess);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
+};
+
+/**
+ * Op Code: EA
+ * 
+ * This operation has no effect on the program.  Moves onto
+ * the next instruction. 
+ */ 
+
 Cpu.prototype.noOperation = function () {
-    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+/**
+ * Op Code: 00
+ * 
+ * Causes the execution of the current running program to finish.
+ */
 Cpu.prototype.breakCall = function () {
     
-};
-Cpu.prototype.compareByteInMemoryWithRegisterX = function () {
+    var pcb = _PCBFactory.getProcess(_CurrentExecutingProcess);
     
+    // Update the appropriate values of the current PCB
+    pcb.setAccumulator(_CPU.Acc);
+    pcb.setProgramCounter(_CPU.PC);
+    pcb.setRegisterX(_CPU.Xreg);
+    pcb.setRegisterY(_CPU.Yreg);
+    pcb.setFlagZ(_CPU.Zflag);
+    // TODO: Set status
+    
+    UIUpdateManager.updateProcessMonitor(_CurrentExecutingProcess);
+    
+     // We are done executing the program, so put the prompt back.
+    _StdIn.advanceLine();
+    _OsShell.putPrompt();
+    
+    // Program requested to stop execution
+    _CPU.isExecuting = false;
 };
+
+/**
+ * Op Code: EC
+ * 
+ * Determines the value of the memory contents at the address
+ * specified by the two byte operand.  Then, compares the value
+ * with the contents of the X register.  Sets the Z flag to 1 if
+ * they are equal.
+ */
+Cpu.prototype.compareByteInMemoryWithRegisterX = function () {
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
+    
+    // Format the bytes as a logical address
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16));
+    
+    // Read the data at the determined logical address.
+    var memoryContents = _MemoryManager.readDataAtLogicalAddress(logicalAddress, _CurrentExecutingProcess);
+    
+    memoryContents = parseInt(memoryContents, 16);
+    
+    // Compare memory contents with the contents of register X
+    if (memoryContents === this.Xreg) {
+        this.Zflag = 1;
+    } else {
+        this.Zflag = 0;
+    }
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
+};
+
+/**
+ * Op Code: D0
+ * 
+ * Branches to an address if the Z Flag is zero.  The address is determined
+ * by the specified operand.
+ */
 Cpu.prototype.branchXBytesIfZFlagIsZero = function () {
     
+    if (this.Zflag === 0) {
+        
+        var branchToAddress = this.fetchNextByteFromMemory();
+        
+        branchToAddress = parseInt(branchToAddress, 16);
+        
+        this.PC = this.PC + branchToAddress;
+        
+        var pcb = _PCBFactory.getProcess(_CurrentExecutingProcess);
+        
+        // Ensure branching out of current process does not occur
+        if (this.PC > pcb.getLimitAddress()) {
+            this.PC = this.PC - SYSTEM_MEMORY_BLOCK_SIZE;
+        };
+        
+        this.incrementProgramCounter();
+    } else {
+        // We are done executing this op code, skip next byte (operand)
+        // by incrementing the PC twice.
+        for (var i=0; i < 2; i++) { this.incrementProgramCounter(); };
+    }
 };
+
+/**
+ * Op Code: EE
+ * 
+ * Determines the memory address to increment based on the next two bytes.
+ * Then, the value at the address is incremented by 1 and it is saved back
+ * to the same (specified) memory address.
+ */
 Cpu.prototype.incrementValueOfAByte = function () {
+    // For this instruction, the address that points to the location in memory
+    // that contains this data is split across two bytes
+    var firstAddressFragment = this.fetchNextByteFromMemory();
+    var secondAddressFragment = this.fetchNextByteFromMemory();
     
+    // Format the bytes as a logical address
+    var logicalAddress = (parseInt(secondAddressFragment, 16) + parseInt(firstAddressFragment, 16));
+    
+    // Convert to decimal
+    var memoryContentsAsBaseTen = parseInt(_MemoryManager.readDataAtLogicalAddress(logicalAddress, _CurrentExecutingProcess), 16);
+    
+    memoryContentsAsBaseTen++;
+    
+    var memoryContentsAsBaseSixteen = memoryContentsAsBaseTen.toString(16).toUpperCase();
+    
+    if (memoryContentsAsBaseSixteen.length === 1) {
+        memoryContentsAsBaseSixteen = "0" + memoryContentsAsBaseSixteen;
+    }
+    
+    // Write it back to memory
+    _MemoryManager.writeDataAtLogicalAddress(logicalAddress, memoryContentsAsBaseSixteen, _CurrentExecutingProcess);
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
+
+// Op Code: FF
 Cpu.prototype.systemCall = function () {
     
+    if (this.Xreg === 1) {
+        
+        var valueOfRegisterX = parseInt(this.Yreg).toString();
+        
+        _StdIn.putText(valueOfRegisterX);        
+        
+    } else if (this.Xreg === 2) {
+        
+        // This is how we know when we reached the end of the system call.
+        var terminationIndicator = "00";
+        
+        // The contents of register Y is an address, translate it.
+        var logicalAddressForRegY = _MemoryManager.translateAddress(_CPU.Yreg, _CurrentExecutingProcess);
+        
+        var currentByte = _MemoryManager.readDataAtLogicalAddress(logicalAddressForRegY, _CurrentExecutingProcess);
+        
+        var textOutput = "";
+        
+        while (currentByte !== terminationIndicator) {
+            var keyCode = parseInt(currentByte, 16);
+            
+            // Add the character to the string
+            textOutput = textOutput + String.fromCharCode(keyCode);
+            
+            // Move to the next logical address
+            logicalAddressForRegY++;
+            
+            // Read the data from memory.
+            currentByte = _MemoryManager.readDataAtLogicalAddress(logicalAddressForRegY, _CurrentExecutingProcess);
+        }
+        // Output the entire string
+        _StdIn.putText(textOutput);
+    }
+    
+   
+    
+    // We are done executing this op code
+    this.incrementProgramCounter();
 };
 
 Cpu.prototype.setCPUProperties = function(programCounter, accumulator, xReg, yReg, zFlag) {
@@ -115,10 +492,10 @@ Cpu.prototype.setCPUProperties = function(programCounter, accumulator, xReg, yRe
 // Called when isExecuting is true
 Cpu.prototype.cycle = function() {
     krnTrace("CPU cycle");
+    
+    // Begin the execution
+    this.executeNextInstruction();
+    
     // TODO: Accumulate CPU usage and profiling statistics here.
     // Do the real work here. Be sure to set this.isExecuting appropriately.
-
-
-    // Refresh the CPU display
-    UIUpdateManager.updateCPUMonitor();
 };
