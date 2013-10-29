3d513c3a211437bd66fa5e9fc8c5ec4d7f456530
Wallance Miranda <wallancem@gmail.com>
2013-10-29 13:45:39 -0400

    Removed the class's properties member and replaced all this.processes statements to use the new _ResidentProcesses global in globals.js.

--- scripts/os/pcb-factory.js
+++ scripts/os/pcb-factory.js
@@ -3,9 +3,8 @@
  * 
  * Responsible for creating and maintaining user processes.
  */
-
-function ProcessControlBlockFactory() {
-    this.processes = null;
+function ProcessControlBlockFactory()
+{
     this.lastProcessID = 0; // Keeps track of the last PID used
     
     this.init();
@@ -14,9 +13,10 @@ function ProcessControlBlockFactory() {
 /**
  * Initalizes the factory prototype.
  */
-ProcessControlBlockFactory.prototype.init = function() {
-    if ((this.processes === null)) {
-        this.processes = new Array();
+ProcessControlBlockFactory.prototype.init = function()
+{
+    if ((_ResidentProcesses === null)) {
+        _ResidentProcesses = new Array();
         this.systemMemory = new MemoryHardware(SYSTEM_MEMORY_SIZE);
     }
 };
@@ -24,9 +24,10 @@ ProcessControlBlockFactory.prototype.init = function() {
 /**
  * Determines where the next base address should be and returns it.
  */
-ProcessControlBlockFactory.prototype.getNextBaseAddress = function() {
-     if(this.processes.length <= SYSTEM_MEMORY_BLOCK_SIZE) {
-        return SYSTEM_MEMORY_BLOCK_SIZE * this.processes.length;
+ProcessControlBlockFactory.prototype.getNextBaseAddress = function()
+{
+     if(_ResidentProcesses.length <= SYSTEM_MEMORY_BLOCK_SIZE) {
+        return SYSTEM_MEMORY_BLOCK_SIZE * _ResidentProcesses.length;
     } else {
         krnTrapError('The next base address exceeds the avaialble physical memory.');
     }
@@ -35,8 +36,9 @@ ProcessControlBlockFactory.prototype.getNextBaseAddress = function() {
 /**
  * Determines where the next limit address should be and returns it.
  */
-ProcessControlBlockFactory.prototype.getNextLimitAddress = function() {
-    if(this.processes.length <= SYSTEM_MEMORY_BLOCK_SIZE) {
+ProcessControlBlockFactory.prototype.getNextLimitAddress = function()
+{
+    if(_ResidentProcesses.length <= SYSTEM_MEMORY_BLOCK_SIZE) {
         // Subtract 1 because we started at 0.
         return this.getNextBaseAddress() + SYSTEM_MEMORY_BLOCK_SIZE - 1;
     } else {
@@ -48,39 +50,47 @@ ProcessControlBlockFactory.prototype.getNextLimitAddress = function() {
  * Only to be used when the PCB is instantiated, and only to be
  * called once per process!
  */
-ProcessControlBlockFactory.prototype.obtainNewProcessID = function() { 
+ProcessControlBlockFactory.prototype.obtainNewProcessID = function()
+{ 
     return this.lastProcessID++;
 };
 
-ProcessControlBlockFactory.prototype.createProcess = function() {
+ProcessControlBlockFactory.prototype.createProcess = function()
+{
     
-    if (this.processes.length <= SYSTEM_MEMORY_BLOCK_COUNT) {
+    if (_ResidentProcesses.length <= SYSTEM_MEMORY_BLOCK_COUNT) {
         
         // First, determine what the properties for the new PCB should be.
         var baseAddress = this.getNextBaseAddress();
         var limitAddress = this.getNextLimitAddress();
-        var memoryBlock = this.processes.length + 1;
+        var memoryBlock = _ResidentProcesses.length + 1;
         
         var pcb = new ProcessControlBlock(this.obtainNewProcessID(), baseAddress, limitAddress, memoryBlock);
         
         // Add the new PCB to the list of processes.
-        this.processes[pcb.getProcessID()] = pcb;
+        _ResidentProcesses[pcb.getProcessID()] = pcb;
         
-        return this.processes[pcb.getProcessID()];
+        return _ResidentProcesses[pcb.getProcessID()];
     
     } else {
         krnTrapError('Could not create process. System only supports ' + SYSTEM_MEMORY_BLOCK_COUNT + '.');
     }
 };
 
-ProcessControlBlockFactory.prototype.getProcess = function(pid) {
+ProcessControlBlockFactory.prototype.getProcess = function(pid)
+{
     // Make sure the process exists.
-    if ( (typeof this.processes[pid] !== 'undefined') || (this.processes[pid] !== null) )
+    if ( (typeof _ResidentProcesses[pid] !== 'undefined') || (_ResidentProcesses[pid] !== null) )
     {
-        return this.processes[pid];
+        return _ResidentProcesses[pid];
     }
     else
     {
         return null;
     }
+};
+
+ProcessControlBlockFactory.prototype.getProcesses = function()
+{
+    return _ResidentProcesses;
 };
