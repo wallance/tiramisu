cbc8219aa61aa12a9ef94ea57431e39c0198a93f
Wallance Miranda <wallancem@gmail.com>
2013-11-05 00:15:38 -0500

    Implemented base and limit registers in the host OS memory code.

--- scripts/host/memory-hardware.js
+++ scripts/host/memory-hardware.js
@@ -5,11 +5,37 @@
 
 function MemoryHardware() {
     this.memory = new Array();
+    
+    this.baseAddressRegister = null;
+    
+    this.limitAddressRegister = null;
+    
+    // Initalize memory as empty
     for (var address=0; address < SYSTEM_MEMORY_SIZE; address++)
-        {
-            this.memory[address] = "00";
-        }
+    {
+        this.memory[address] = "00";
+    }
 }
+
+MemoryHardware.prototype.setBaseRegister = function(address)
+{
+    this.baseAddressRegister = address;
+};
+
+MemoryHardware.prototype.setLimitRegister = function(address)
+{
+    this.limitAddressRegister = address;
+};
+
+MemoryHardware.prototype.getBaseRegister = function()
+{
+    return this.baseAddressRegister;
+};
+
+MemoryHardware.prototype.getLimitRegister = function()
+{
+    return this.limitAddressRegister;
+};
 
 MemoryHardware.prototype.read = function(address)
 {
