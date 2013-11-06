441fcf3d80396387370f031c3864019fbcc3d5b6
Wallance Miranda <wallancem@gmail.com>
2013-11-06 08:35:05 -0500

    Added the schedule method.  It determines when to switch processes.

--- scripts/os/scheduler.js
+++ scripts/os/scheduler.js
@@ -13,3 +13,33 @@ Scheduler.prototype.init = function()
 {
     _RoundRobinCycleCount = 1;
 };
+
+Scheduler.prototype.schedule = function()
+{
+    // Schedule the next process in the ready queue by removing it.
+    _CurrentExecutingProcess = _ReadyQueue.dequeue();
+    
+    // Retrieve the Process Control Block (PCB).
+    var currentProcess = _PCBFactory.getProcess(_CurrentExecutingProcess);
+    
+    if ( (_RoundRobinCycleCount > _RoundRobinQuantum) && (_ReadyQueue.getNext()) )
+    {
+        // Switch when the process has been executed the same number of times
+        // as the quantum.
+        this.switchContext();
+    }
+    else if (currentProcess.getState() === "Terminated")
+    {
+        this.switchContext();
+    }
+};
+
+Scheduler.prototype.switchContext = function()
+{
+   
+};
+
+Scheduler.prototype.setMode = function(mode)
+{
+    _Mode = mode;
+};
