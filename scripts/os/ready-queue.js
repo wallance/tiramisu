5c3f22bf65156c5ff771039dd5ee125a1c3020ed
Wallance Miranda <wallancem@gmail.com>
2013-11-07 01:15:45 -0500

    Implemented a separate version of the queue for use as a Ready Queue to make UI updating simpler and more manageable.

---
+++ scripts/os/ready-queue.js
@@ -0,0 +1,60 @@
+/* ------------
+   Queue.js
+   
+   A simple Queue, which is really just a dressed-up JavaScript Array.
+   See the Javascript Array documentation at http://www.w3schools.com/jsref/jsref_obj_array.asp .
+   Look at the push and shift methods, as they are the least obvious here.
+   
+   ------------ */
+   
+function ReadyQueue()
+{
+    // Properties
+    this.q = new Array();
+
+    // Methods
+    this.getSize = function() {
+        return this.q.length;    
+    };
+
+    this.isEmpty = function(){
+        return (this.q.length === 0);    
+    };
+
+    this.enqueue = function(pid) {
+        this.q.push(pid);
+        UIUpdateManager.updateReadyQueue(pid);
+    };
+    
+    this.dequeue = function() {
+        var retVal = null;
+        
+        if (this.q.length > 0)
+        {
+            retVal = this.q.shift();
+            UIUpdateManager.dequeueItemFromReadyQueueMonitor();
+        }
+        return retVal;        
+    };
+    
+    this.toString = function() {
+        var retVal = "";
+        for (var i in this.q)
+        {
+            retVal += "[" + this.q[i] + "] ";
+        }
+        return retVal;
+    };
+    
+    this.getNext = function() {
+        if (this.getSize() > 0)
+        {
+            // We don't want to remove it, only return the item
+            return this.q[0];
+        }
+        else
+        {
+            return null;
+        }
+    };
+}
