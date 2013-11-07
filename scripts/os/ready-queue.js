9a2869d0b49479fd1cc07b1dfce9ca5fe1ded6dd
Wallance Miranda <wallancem@gmail.com>
2013-11-07 01:17:24 -0500

    Added comments.

--- scripts/os/ready-queue.js
+++ scripts/os/ready-queue.js
@@ -1,9 +1,12 @@
 /* ------------
-   Queue.js
+   ReadyQueue.js
    
    A simple Queue, which is really just a dressed-up JavaScript Array.
    See the Javascript Array documentation at http://www.w3schools.com/jsref/jsref_obj_array.asp .
    Look at the push and shift methods, as they are the least obvious here.
+
+   This version of the JS queue is uses exclusively for the Ready Queue for OS
+   scheduling. It allows us to easily update the UI when necessary.
    
    ------------ */
    
@@ -23,6 +26,7 @@ function ReadyQueue()
 
     this.enqueue = function(pid) {
         this.q.push(pid);
+        // Update the web UI.
         UIUpdateManager.updateReadyQueue(pid);
     };
     
@@ -32,6 +36,7 @@ function ReadyQueue()
         if (this.q.length > 0)
         {
             retVal = this.q.shift();
+            // Update the web UI.
             UIUpdateManager.dequeueItemFromReadyQueueMonitor();
         }
         return retVal;        
