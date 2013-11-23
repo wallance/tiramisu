7423511633169660057d8721267701605c74104b
Wallance Miranda <wallancem@gmail.com>
2013-11-23 14:00:25 -0500

    Adding the file system driver to git.

---
+++ scripts/os/device-driver-file-system.js
@@ -0,0 +1,9 @@
+/* ----------------------------------
+   device-driver-file-system.js
+   
+   Requires deviceDriver.js
+   
+   The File System Device Driver.
+   ---------------------------------- */
+
+DeviceDriverFileSystem.prototype = new DeviceDriver;  // "Inherit" from prototype DeviceDriver in deviceDriver.js.
