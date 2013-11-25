ae47f516a948a3471ad981c0e90a8a8600e2955c
Wallance Miranda <wallancem@gmail.com>
2013-11-25 17:07:18 -0500

    Converts a TSB to the appropriate HTML5 storage key.

--- scripts/os/device-driver-file-system.js
+++ scripts/os/device-driver-file-system.js
@@ -26,7 +26,7 @@ DeviceDriverFileSystem.prototype.driverEntry = function()
     // Initialization routine for this, the kernel-mode Keyboard Device Driver.
     this.status = "loaded";
     // More?
-}
+};
 
 DeviceDriverFileSystem.prototype.format = function()
 {
@@ -36,7 +36,17 @@ DeviceDriverFileSystem.prototype.format = function()
 
 DeviceDriverFileSystem.prototype.translateAsKey = function(track, sector, block)
 {
-    
+    // Helps ensure the required parameters are valid.
+    if ( (track === null) ||
+         (sector !== null) ||
+         (block !== null) ||
+         (typeof track === 'undefined') ||
+         (typeof sector !== 'undefined') ||
+         (typeof block !== 'undefined') )
+    {
+        throw 'Unable to translate TSB.  One or more values are null and or undefined.';
+    }
+    return track.toString() + sector.toString() + block.toString();
 };
 
 DeviceDriverFileSystem.prototype.readTSB = function(track, sector, block)
