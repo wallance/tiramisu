aa7f5edea4e1a1eba14bb84d5f3b8d0d12951e30
Wallance Miranda <wallancem@gmail.com>
2013-11-25 16:28:53 -0500

    Added basic methods for file system driver.  Created class constants for TSB information.

--- scripts/os/device-driver-file-system.js
+++ scripts/os/device-driver-file-system.js
@@ -4,6 +4,57 @@
    Requires deviceDriver.js
    
    The File System Device Driver.
+
+   This driver utilizes the HTML5 Session Storage object to implement its
+   functionality.
    ---------------------------------- */
 
 DeviceDriverFileSystem.prototype = new DeviceDriver;  // "Inherit" from prototype DeviceDriver in deviceDriver.js.
+
+function DeviceDriverFileSystem()
+{
+    this.TRACK_COUNT = 3;
+    this.SECTOR_COUNT = 7;
+    this.BLOCK_SIZE_IN_BYTES = 64;
+    this.BLOCKS_PER_SECTOR = 7;
+    
+    this.hardDisk = sessionStorage;
+};
+
+DeviceDriverFileSystem.prototype.driverEntry = function()
+{
+    // Initialization routine for this, the kernel-mode Keyboard Device Driver.
+    this.status = "loaded";
+    // More?
+}
+
+DeviceDriverFileSystem.prototype.format = function()
+{
+    this.hardDisk.clear();
+    
+};
+
+DeviceDriverFileSystem.prototype.translateAsKey = function(track, sector, block)
+{
+    
+};
+
+DeviceDriverFileSystem.prototype.readTSB = function(track, sector, block)
+{
+    return this.hardDisk.getItem(this.translateAsKey(track, sector, block));
+};
+
+DeviceDriverFileSystem.prototype.writeToTSB = function(track, sector, block, data)
+{
+    this.hardDisk.setItem(this.translateAsKey(track, sector, block), data);
+};
+
+DeviceDriverFileSystem.prototype.deleteObjectAtPath = function()
+{
+    
+};
+
+DeviceDriverFileSystem.prototype.createNewFile = function()
+{
+    
+};
